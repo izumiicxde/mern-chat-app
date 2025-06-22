d76cfaf50d2826aa8a0ffc807145ece51dac8efa
izumii <154342465+izumiCode@users.noreply.github.com>
2025-06-22 22:16:19 +0530

    feat(routes): add GET routes for fetching users and messages, and POST route for sending a message

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -10,7 +11,9 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running at port: ${PORT}`);
