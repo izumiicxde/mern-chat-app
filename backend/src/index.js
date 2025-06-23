e99a0807c85be38b88eb44a11e1994206b521f82
izumii <154342465+izumiCode@users.noreply.github.com>
2025-06-23 13:39:06 +0530

    chore: BACKEND: setup cors for the site. Correct the response code of unathorized access

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -11,7 +12,16 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "site is up" });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -6,17 +6,17 @@ export const protectRoute = async (req, res, next) => {
     const token = req.cookies.jwt;
     if (!token)
       return res
-        .status(400)
+        .status(401)
         .json({ message: "Unauthorized - No Token Provided" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded)
-      return res.status(400).json({ message: "Unauthorized - Invalid Token." });
+      return res.status(401).json({ message: "Unauthorized - Invalid Token." });
 
     const user = await User.findById(decoded.userId).select("-password");
     if (!user)
       return res
-        .status(400)
+        .status(401)
         .json({ message: "Unauthorized - User not found." });
 
     req.user = user;
